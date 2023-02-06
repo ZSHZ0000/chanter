152dbcffa0775bedfc687556e30e1de570bbb2e9
Joshua Pc. Hdz <zcheeze0@gmail.com>
2023-02-06 15:26:58 -0600

    Cleaned up the virtual machine prototype somewhat, changed opcodes.
    Rationale: I believed that I may need to add some extra functions so I
    reserved some space to add them in any case I actually end up needing
    to add them, such ones are 4 bytes index constant loading & 4 bytes
    stack referencing.
    
    lisp/assembly.lisp: Modified bytecode of several instructions.
    lisp/vm.lisp: Cleaned up the virtual machine, reflect changes of the
    assembler.

--- lisp/assembly.lisp
+++ lisp/assembly.lisp
@@ -4,29 +4,39 @@
 (defpackage #:chanter.asm
   (:use #:cl)
   (:export #:*instruction-set*
-		   #:assembly-1))
+		   #:assemble-1))
 (in-package #:chanter.asm)
 
 ;;; Instruction set, it must have the form (MNEMONIC OPCODE ARG-COUNT &REST ARG-SIZE)
 (defvar *instruction-set*
   '((RETURN 0 0)
+	;; Stack operations.
 	(DUP 1 0)
 	(POP 2 0)
-	(CONST 3 1 1)
-	(STACK-REF 4 1 1)
-	(ADD 5 0)
-	(SUB 6 0)
-	(MUL 7 0)
-	(DIV 8 0)))
+	(CONST 3 1 2)
+	(STACK-REF 4 1 2)
+	;; Arithmetic operations.
+	(ADD 7 0)
+	(SUB 8 0)
+	(MUL 9 0)
+	(DIV 10 0)
+	;; Comparison operations.
+	(EQ? 13 0)
+	(NEQ? 14 0)
+	(ZERO? 15 0)
+	(LESSER? 16 0)
+	(GREATER? 17 0)
+	(LESSEQ? 18 0)
+	(GREATEQ? 19 0)))
 
-;;; Various abstractions.
+;;; Various general instruction abstractions.
 (defun get-data (mnemonic)
   (assoc mnemonic *instruction-set* :test #'string=))
 (defun get-opcode (mnemonic)
   (second (get-data mnemonic)))
-(defun get-arg-count (mnemonic)
+(defun get-argument-count (mnemonic)
   (third (get-data mnemonic)))
-(defun get-arg-sizes (mnemonic)
+(defun get-argument-sizes (mnemonic)
   (cdddr (get-data mnemonic)))
 
 ;;; Emit byte.
@@ -41,10 +51,6 @@
 	  ((= bytes-done byte-count) output)
 	(push (ldb (byte 8 offset) fixnum) output)))
 
-;; Add label.
-(defun add-label (label hash-table program-counter)
-  (setf (gethash label hash-table) program-counter))
-
 ;;; Assembler
 (defun assemble-1 (the-constants the-instructions)
   "Emit byte-code based on the given instructions & constants"
@@ -54,7 +60,6 @@
 		(the-constants-output (make-array 32 :element-type t
 											 :adjustable t
 											 :fill-pointer 0))
-		(label-addresses (make-hash-table :test #'string=))
 		(virtual-prog-counter 0))
 	;; Create the constants vector.
 	(dolist (constant the-constants)
@@ -67,37 +72,38 @@
 		 (let* ((mnemonic (first instruction))
 				(arguments (rest instruction))
 				(opcode-byte (get-opcode mnemonic))
-				(opcode-arg-count (get-arg-count mnemonic)))
+				(opcode-argument-count (get-argument-count mnemonic)))
 		   (cond
 			 ;; If it is an argument-less instruction just emit it right away.
-			 ((= opcode-arg-count 0)
+			 ((= opcode-argument-count 0)
 			  (when arguments
 				(warn
-				 (format nil "Instruction ~A that is argument-less has arguments ~A"
+				 (format nil "Instruction ~A that is argument-less has arguments ~A."
 						 mnemonic arguments)))
 			  (emit-byte the-bytecode-output opcode-byte)
 			  (incf virtual-prog-counter))
 			 ;; If the argument length isn't equal to the arguments required by the
 			 ;; instruction, error out.
-			 ((/= opcode-arg-count (length arguments))
+			 ((/= opcode-argument-count (length arguments))
 			  (error
-			   (format nil "Instruction ~A requires specifically ~A arguments but got ~A"
-					   mnemonic opcode-arg-count (length arguments))))
+			   (format nil "Instruction ~A requires specifically ~A arguments but got ~A."
+					   mnemonic opcode-argument-count (length arguments))))
 			 ;; instruction with matching argument count.
 			 (t
 			  (emit-byte the-bytecode-output opcode-byte)
 			  (incf virtual-prog-counter)
 			  ;; Process arguments.
-			  (do* ((args-left arguments (rest arg-sizes-left))
-					(arg-sizes-left (get-arg-sizes mnemonic) (rest arg-sizes-left))
-					(arg (first args-left) (first args-left))
-					(arg-size (first arg-sizes-left) (first arg-sizes-left)))
-				   ((not args-left))
-				;; Emit the argument bytes byte by byte.
-				(dolist (byte (byte-transform arg arg-size))
+			  (do* ((arguments-left arguments (rest argument-sizes-left))
+					(argument-sizes-left (get-argument-sizes mnemonic) (rest argument-sizes-left))
+					(arguments (first arguments-left) (first arguments-left))
+					(arguments-size (first argument-sizes-left) (first argument-sizes-left)))
+				   ;; Quit when there are no more argumentsuments left.
+				   ((null arguments-left))
+				;; Emit the argumentsument bytes byte by byte.
+				(dolist (byte (byte-transform arguments arguments-size))
 				  (emit-byte the-bytecode-output byte)
 				  (incf virtual-prog-counter)))))))
-		;; Otherwise it is a label.
-		((keywordp instruction)
-		 (add-label instruction label-addresses virtual-prog-counter))))
-	(vector the-bytecode-output the-constants-output label-addresses)))
+		;; Otherwise error out.
+		(t
+		 (error "Bad type passed to the assembler."))))
+	(vector the-bytecode-output the-constants-output)))
